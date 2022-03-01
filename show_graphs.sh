cb42f971ddaa416affa34e4265c5af5808f03f44
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2022-03-01 16:05:10 +0000

    Add quick and dirty help to show_graphs.sh

--- show_graphs.sh
+++ show_graphs.sh
@@ -13,11 +13,15 @@ while getopts "t:p:g:H:S" arg; do
 done
 shift $((OPTIND - 1))
 
+HELPER="-t `ls secrets/graphs/${HASH}/class_times`"
 for TYPE in "${TYPES[@]}"; do
+  HELPER="-p `ls secrets/graphs/${HASH}/class_times/${TYPE}`"
   for POPULATION in "${POPULATIONS[@]}"; do
+    HELPER="-g `ls secrets/graphs/${HASH}/class_times/${TYPE}/${POPULATION}/dynamic | sed 's/\.html//g'`"
     for GRAPH in "${GRAPHS[@]}"; do
       #echo secrets/graphs/"${HASH}"/class_times/"${TYPE}"/"${POPULATION}"/ -name "*${GRAPH}.html" >&2
       find secrets/graphs/"${HASH}"/class_times/"${TYPE}"/"${POPULATION}"/ -name "*${GRAPH}.html"
     done | sort -t / -g -k 5,6 -k 6,7 -k 8  | { if [ $SHOW -ne 1 ]; then xargs google-chrome --new-window & else echo "${TYPE^^}" "${POPULATION^^}"; tee; fi }
   done
 done
+echo $HELPER
