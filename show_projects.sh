fcc6af3f3e9b189edabf87a789fd7b8807a49691
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2022-03-18 18:50:47 +0000

    Make 'show' scripts more precise
    If files are missing, it looks like Chrome will try to guess a
    URL based on the path. Making the path an absolute file URL will
    stop this from happening.

--- show_projects.sh
+++ show_projects.sh
@@ -2,7 +2,7 @@
 #e.g. ./show_projects.sh will show the overview
 #     ./show_projects.sh _q[34] will show the top/other quartiles breakdown
 google-chrome --new-window \
-  secrets/graphs/`git rev-parse HEAD`/class_times/project/all_classifiers/dynamic/HMS_NHS${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/project/all_classifiers/dynamic/Scarlets_and_Blues${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/project/all_classifiers/dynamic/RBGE_Herbarium${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/project/all_classifiers/dynamic/HMS_NHS${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/project/all_classifiers/dynamic/Scarlets_and_Blues${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/project/all_classifiers/dynamic/RBGE_Herbarium${1}.html \
   &
--- show_workflows.sh
+++ show_workflows.sh
@@ -2,22 +2,22 @@
 #e.g. ./show_projects.sh will show the overview
 #     ./show_projects.sh _q[34] will show the top/other quartiles breakdown
 google-chrome --new-window \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/1._ADMISSION_NUMBER${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/5._AGE${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/14._NUMBER_OF_DAYS_IN_HOSPITAL${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/YEARS_AT_SEA${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/2._DATE_OF_ENTRY${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/12._DATE_OF_DISCHARGE${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/QUALITY_\(DD\)${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/HOW_DISPOSED_OF_\(DD\)${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/7._PLACE_OF_BIRTH_NATIONALITY${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/10._WHERE_FROM${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/3._NAME${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/8._SHIP_SHIP_OR_PLACE_OF_EMPLOYMENT_LAST_SHIP${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/11._NATURE_OF_COMPLAINT${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/People${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/attendance${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/Meetings${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/Latitude_Longitude${1}.html \
-  secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/Geography${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/1._ADMISSION_NUMBER${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/5._AGE${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/14._NUMBER_OF_DAYS_IN_HOSPITAL${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/YEARS_AT_SEA${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/2._DATE_OF_ENTRY${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/12._DATE_OF_DISCHARGE${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/QUALITY_\(DD\)${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/HOW_DISPOSED_OF_\(DD\)${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/7._PLACE_OF_BIRTH_NATIONALITY${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/10._WHERE_FROM${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/3._NAME${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/8._SHIP_SHIP_OR_PLACE_OF_EMPLOYMENT_LAST_SHIP${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/11._NATURE_OF_COMPLAINT${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/People${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/attendance${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/Meetings${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/Latitude_Longitude${1}.html \
+  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow/all_classifiers/dynamic/Geography${1}.html \
   &
