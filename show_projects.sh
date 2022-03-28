03a963fc0de7f679aade3fc7a460f91fbfeb96c2
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2022-03-28 16:20:38 +0100

    Override hash from CLI in all show_* scripts

--- show_projects.sh
+++ show_projects.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 #e.g. ./show_projects.sh will show the overview
 #     ./show_projects.sh _q[34] will show the top/other quartiles breakdown
+HASH="`git rev-parse HEAD`"
+while getopts "H:" arg; do
+  case "${arg}" in
+    H) HASH="${OPTARG}";;
+  esac
+done
+shift $((OPTIND - 1))
+
 google-chrome --new-window \
-  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/project/all_classifiers/dynamic/HMS_NHS${1}.html \
-  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/project/all_classifiers/dynamic/Scarlets_and_Blues${1}.html \
-  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/project/all_classifiers/dynamic/RBGE_Herbarium${1}.html \
+  file://"${PWD}"/secrets/graphs/"${HASH}"/class_times/project/all_classifiers/dynamic/HMS_NHS${1}.html \
+  file://"${PWD}"/secrets/graphs/"${HASH}"/class_times/project/all_classifiers/dynamic/Scarlets_and_Blues${1}.html \
+  file://"${PWD}"/secrets/graphs/"${HASH}"/class_times/project/all_classifiers/dynamic/RBGE_Herbarium${1}.html \
   &
--- show_types.sh
+++ show_types.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 #e.g. ./show_projects.sh will show the overview
 #     ./show_projects.sh _q[34] will show the top/other quartiles breakdown
+HASH="`git rev-parse HEAD`"
+while getopts "H:" arg; do
+  case "${arg}" in
+    H) HASH="${OPTARG}";;
+  esac
+done
+shift $((OPTIND - 1))
+
 google-chrome --new-window \
-  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow_type/all_classifiers/dynamic/col-num${1}.html \
-  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow_type/all_classifiers/dynamic/col-date${1}.html \
-  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow_type/all_classifiers/dynamic/col-dropdown${1}.html \
-  file://"${PWD}"/secrets/graphs/`git rev-parse HEAD`/class_times/workflow_type/all_classifiers/dynamic/col-noun${1}.html \
+  file://"${PWD}"/secrets/graphs/"${HASH}"/class_times/workflow_type/all_classifiers/dynamic/col-num${1}.html \
+  file://"${PWD}"/secrets/graphs/"${HASH}"/class_times/workflow_type/all_classifiers/dynamic/col-date${1}.html \
+  file://"${PWD}"/secrets/graphs/"${HASH}"/class_times/workflow_type/all_classifiers/dynamic/col-dropdown${1}.html \
+  file://"${PWD}"/secrets/graphs/"${HASH}"/class_times/workflow_type/all_classifiers/dynamic/col-noun${1}.html \
   &
