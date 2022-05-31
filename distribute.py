b79707d04d195b1f570f447c4f68c556cb445eec
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2022-05-31 16:17:30 +0100

    Maintain the hash-dir when copying graphs for data sharing platform
    Take the opportunity to factor out all the calls to u.git_HEAD()

--- distribute.py
+++ distribute.py
@@ -12,19 +12,21 @@ with tempfile.TemporaryDirectory(dir = '.') as tmpdir:
   copy('all_classifications.csv', inner_dir)
   copy('README_all_classifications', f'{inner_dir}/README.txt')
 
-  copytree(f'secrets/graphs/{u.git_HEAD()}/', f'{inner_dir}/graphs', symlinks = True, ignore = ignore_patterns('static'), copy_function = os.link)
+  git_hash = u.git_HEAD()
+  os.mkdir(f'{inner_dir}/graphs')
+  copytree(f'secrets/graphs/{git_hash}/', f'{inner_dir}/graphs/{git_hash}/', symlinks = True, ignore = ignore_patterns('static'), copy_function = os.link)
   with open(f'{inner_dir}/README.txt', 'a') as f:
     f.write(f'''
 To regenerate these charts:
 * git clone https://github.com/nationalarchives/engagingcrowds_engagement.git
 * cd engagingcrowds_engagement
-* git co {u.git_HEAD()} #git co 97cedb41511b9494b750152b26d91cfe24bd46d1 to use the same version as the charts in the report
+* git co {git_hash} #git co 97cedb41511b9494b750152b26d91cfe24bd46d1 to use the same version as the charts in the report
 * pip install pandas@1.4.1  #You might prefer to do this in a virtualenv
 * pip install plotly@5.6.0  #You might prefer to do this in a virtualenv
 * pip install kaleido@0.2.1 #You might prefer to do this in a virtualenv
 * wget https://tanc-ahrc.github.io/EngagingCrowds/data/all_classifications.csv
 * ./analyse.py
-Graphs will appear in secrets/graphs/{u.git_HEAD()}
+Graphs will appear in secrets/graphs/{git_hash}
 The CSV files can be used to confirm that the graphs are based upon the same input data
 The overall run log and the *_desc.txt files contain additional data.
 
