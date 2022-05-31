b1e2c2e5fb26f42baa284b5eca2e5d364fba4f1e
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2022-05-31 14:54:12 +0100

    Remove ad-hoc image galleries
    We have opted to direct link to the projects to provide image
    galleries.

--- distribute.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./galleries/gen_hms_gallery.sh
-./galleries/gen_sb_gallery.sh
-./galleries/gen_rbge_gallery.sh
-./distribute.py
--- galleries/gen_sb_gallery.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cat "`dirname $0`"/header.html > "`dirname $0`"/../sharing/sb_gallery.html
-csvtool namedcols subj.image,location <(tar xf sharing/Scarlets_and_Blues.tar.xz ./Scarlets_and_Blues_data/classifications.csv -O)  | sed 1d | sort -t, -k1,1 -u | csvtool col 2 - | sed 's#.*#{type: "image", url: "&"},#' >> "`dirname $0`"/../sharing/sb_gallery.html
-cat "`dirname $0`"/footer.html >> "`dirname $0`"/../sharing/sb_gallery.html
