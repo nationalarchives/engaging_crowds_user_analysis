fe228e266f85fa1c1e403160f55d85329d2fed15
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2022-05-31 15:13:07 +0100

    Add reproduction note to 'engagament data' README
    Just records how to get the charts as produced in the report.

--- distribute.py
+++ distribute.py
@@ -16,7 +16,7 @@ with tempfile.TemporaryDirectory(dir = '.') as tmpdir:
 To regenerate these charts:
 * git clone https://github.com/nationalarchives/engagingcrowds_engagement.git
 * cd engagingcrowds_engagement
-* git co {u.git_HEAD()}
+* git co {u.git_HEAD()} #git co 97cedb41511b9494b750152b26d91cfe24bd46d1 to use the same version as the charts in the report
 * pip install pandas@1.4.1  #You might prefer to do this in a virtualenv
 * pip install plotly@5.6.0  #You might prefer to do this in a virtualenv
 * pip install kaleido@0.2.1 #You might prefer to do this in a virtualenv
